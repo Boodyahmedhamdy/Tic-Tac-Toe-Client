8a3c204717027d9d13ab6a6e800a02656c6bf59d
Abdelrahman Mohamed Elshreif <abdelrahman31009246@f-eng.tanta.edu.eg>
2025-01-08 12:51:09 +0200

    Adding The Promopt Of Connecting Server with It's IP - Validating The IP as it should be valid

--- src/tictactoe/client/StartOptionsScreenController.java
+++ src/tictactoe/client/StartOptionsScreenController.java
@@ -1,13 +1,19 @@
 package tictactoe.client;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.application.Application;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -15,7 +21,7 @@ import javafx.scene.control.TextInputDialog;
  * @author Abdelrahman_Elshreif
  */
 public class StartOptionsScreenController implements Initializable {
-    
+
     @FXML
     private Label gameTitle;
     @FXML
@@ -26,9 +32,6 @@ public class StartOptionsScreenController implements Initializable {
     private Button playOnlineBtn;
     @FXML
     private Button prevRecordsBtn;
-    
-    @FXML
-    TextInputDialog dialog = new TextInputDialog();
 
     /**
      * Initializes the controller class.
@@ -39,19 +42,48 @@ public class StartOptionsScreenController implements Initializable {
         playWithAIbtn.setOnAction((event) -> {
             System.out.println("هخش دلوقتى احط على البووت");
         });
-        
+
         playWithFreindBtn.setOnAction((event) -> {
             System.out.println("هسحققققك");
         });
-        
-        playOnlineBtn.setOnAction((event) -> {
-            System.out.println("يلاا بينا اونلاين ");
+
+        playOnlineBtn.setOnAction((outerevent) -> {
+            Alert iperror = new Alert(AlertType.ERROR);
+            TextInputDialog ipPicker = new TextInputDialog("");
+
+            ipPicker.setHeaderText("Please Enter Server IP");
+            ipPicker.setTitle("Connect to Server");
+            ipPicker.setContentText("IP:");
+            ipPicker.getDialogPane().setPrefSize(400, 250);
+
+            Image image = new Image(getClass().getResourceAsStream("ui/styles/online-game-svgrepo-com.png"));
+            ImageView imgView = new ImageView(image);
+            imgView.setFitHeight(40);
+            imgView.setFitWidth(40);
+            ipPicker.setGraphic(imgView);
+
+            Optional<String> result = ipPicker.showAndWait();
+            result.ifPresent(ipAddress -> {
+                if (!isValidIpAddress(ipAddress)) {
+                    iperror.setAlertType(AlertType.ERROR);
+                    iperror.setTitle("Connection Failed");
+                    iperror.setHeaderText("INVALID IP");
+                    iperror.showAndWait();
+                }
+
+            });
+
         });
-        
+
         prevRecordsBtn.setOnAction((event) -> {
             System.out.println("سيديهاتك كلها هنااا اوعى تنسى نفسك");
         });
-        
+
     }
-    
+
+    private boolean isValidIpAddress(String ipAddress) {
+        // Simple regex for IPv4 address validation
+        String regex = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+        return ipAddress.matches(regex);
+    }
 }

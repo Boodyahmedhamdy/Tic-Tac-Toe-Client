c07d4d4f2ddc19228953d878e7328aa9c2510ff9
yomna ashraf <yomna.ashraf559@gmail.com>
2025-01-12 15:57:08 +0200

    Login&Register Validation

--- src/tictactoe/client/TicTacToeClient.java
+++ src/tictactoe/client/TicTacToeClient.java
@@ -14,7 +14,7 @@ public class TicTacToeClient extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("StartOptionsScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
 
 
    //     Parent root = FXMLLoader.load(getClass().getResource("StartOptionsScreen.fxml"));

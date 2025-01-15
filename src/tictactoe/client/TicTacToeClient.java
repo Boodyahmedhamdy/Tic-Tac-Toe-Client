5eaecd1588be2102227c6111d9953b6f8bd48464
Rofaida <rofaidasobhy123@gmail.com>
2025-01-16 01:11:43 +0200

    create win video in local mode play with friend

--- src/tictactoe/client/TicTacToeClient.java
+++ src/tictactoe/client/TicTacToeClient.java
@@ -1,8 +1,13 @@
 package tictactoe.client;
+import java.io.File;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 /**
@@ -20,16 +25,38 @@ public class TicTacToeClient extends Application {
 
 
 
-        Parent root = FXMLLoader.load(getClass().getResource("StartOptionsScreen.fxml"));
+       // Parent root = FXMLLoader.load(getClass().getResource("StartOptionsScreen.fxml"));
 
 
 //        Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
-//         Parent root = FXMLLoader.load(getClass().getResource("gameScreen.fxml"));
+         Parent root = FXMLLoader.load(getClass().getResource("gameScreen.fxml"));
 
 
 
        // Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
 
+       
+       /* String path = "D:/Rofaida Sobhy/java/TicTacToe/Tic-Tac-Toe-Client/src/tictactoe/client/win-video.mp4";  
+          
+        //Instantiating Media class  
+        Media media = new Media(new File(path).toURI().toString());  
+          
+        //Instantiating MediaPlayer class   
+        MediaPlayer mediaPlayer = new MediaPlayer(media);  
+          
+        //Instantiating MediaView class   
+        MediaView mediaView = new MediaView(mediaPlayer);  
+        
+        //by setting this property to true, the Video will be played   
+        mediaPlayer.setAutoPlay(true);  
+          
+        //setting group and scene   
+        Group root = new Group();
+        root.getChildren().add(mediaView);  
+        Scene scene = new Scene(root,500,400);  
+        stage.setScene(scene);  
+        stage.setTitle("Playing video");  
+        stage.show();  */
 
         Scene scene = new Scene(root);
 

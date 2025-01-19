5e84a9f50d5c729bbf62a943a37c1734594af694
Abdelrahman Mohamed Elshreif <abdelrahman31009246@f-eng.tanta.edu.eg>
2025-01-19 13:47:36 +0200

    Trying to record

--- src/tictactoe/client/StartOptionsScreenController.java
+++ src/tictactoe/client/StartOptionsScreenController.java
@@ -21,6 +21,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import tictactoe.client.PlayerSocket;
 
 public class StartOptionsScreenController implements Initializable {
 
@@ -45,6 +46,7 @@ public class StartOptionsScreenController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         setupButtonActions();
+
     }
 
     private void handlePlayWithAI() {
@@ -62,19 +64,7 @@ public class StartOptionsScreenController implements Initializable {
     private void handlePlayOnline() {
         Alert ipError = new Alert(AlertType.ERROR);
         TextInputDialog ipPicker = new TextInputDialog("");
-
-        applyStyles(ipPicker, ipError);
-
-        ipPicker.setHeaderText("Please Enter Server IP");
-        ipPicker.setTitle("Connect to Server");
-        ipPicker.setContentText("IP:");
-        ipPicker.getDialogPane().setPrefSize(400, 250);
-
-        Image image = new Image(getClass().getResourceAsStream("ui/styles/online-game-svgrepo-com.png"));
-        ImageView imgView = new ImageView(image);
-        imgView.setFitHeight(40);
-        imgView.setFitWidth(40);
-        ipPicker.setGraphic(imgView);
+        ipPickerDialogCustomize(ipPicker, ipError);
 
         Optional<String> result = ipPicker.showAndWait();
         result.ifPresent(ip -> {
@@ -83,7 +73,6 @@ public class StartOptionsScreenController implements Initializable {
             } else {
                 try {
                     if (connectToServer(ip)) {
-                        PlayerSocket playerSocket = new PlayerSocket();
                         navigateToScreen("LoginScreen.fxml", playOnlineBtn);
                     } else {
                         showErrorAlert(ipError, "Cannot Find Server with This IP", "Connection Failed");
@@ -103,32 +92,36 @@ public class StartOptionsScreenController implements Initializable {
         }
     }
 
-    private boolean isValidIpAddress(String ipAddress) {
-        String regex = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
-        return ipAddress.matches(regex);
-    }
+    public boolean connectToServer(String serverIP) {
+        System.out.println("Player Initiating Connection");
+        Alert connecting = new Alert(AlertType.INFORMATION);
+        connecting.getDialogPane().getStylesheets().add(getClass().getResource("ui/styles/Alert_Dialogs_Style.css").toExternalForm());
+        connecting.setTitle("Connecting");
+        connecting.setHeaderText("Please Wait.....");
+        connecting.show();
 
-    private boolean connectToServer(String serverIP) {
         try {
-            System.out.println("Player Initiating Connection");
-            Alert connecting = new Alert(AlertType.INFORMATION);
-            connecting.getDialogPane().getStylesheets().add(getClass().getResource("ui/styles/Alert_Dialogs_Style.css").toExternalForm());
-            connecting.setTitle("Connecting");
-            connecting.setHeaderText("Please Wait.....");
-            connecting.show();
-
             InetSocketAddress serverAddress = new InetSocketAddress(serverIP, 9800);
-            Socket socket = new Socket();
-            socket.connect(serverAddress, 1000); // Timeout of 1 second
+            PlayerSocket socket = new PlayerSocket();
+            boolean connected = socket.connect(serverAddress, 1000); // Timeout of 1 second
             connecting.close();
-            return true;
-        } catch (IOException ex) {
-            Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, null, ex);
+            if (connected) {
+                socket.startCommunication();
+            }
+            return connected;
+        } catch (Exception e) {
+            connecting.close();
+            Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, "Connection failed", e);
             return false;
         }
     }
 
-    private void navigateToScreen(String fxmlFile, Button b) throws IOException {
+    public boolean isValidIpAddress(String ipAddress) {
+        String regex = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+        return ipAddress.matches(regex);
+    }
+
+    public void navigateToScreen(String fxmlFile, Button b) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
         Parent root = loader.load();
         Stage stage = (Stage) b.getScene().getWindow();
@@ -136,7 +129,7 @@ public class StartOptionsScreenController implements Initializable {
         stage.show();
     }
 
-    private void showErrorAlert(Alert alert, String headerText, String title) {
+    public void showErrorAlert(Alert alert, String headerText, String title) {
         alert.setAlertType(AlertType.ERROR);
         alert.setTitle(title);
         alert.setHeaderText(headerText);
@@ -146,5 +139,19 @@ public class StartOptionsScreenController implements Initializable {
     private void applyStyles(TextInputDialog ipPicker, Alert alert) {
         ipPicker.getDialogPane().getStylesheets().add(getClass().getResource("ui/styles/Alert_Dialogs_Style.css").toExternalForm());
         alert.getDialogPane().getStylesheets().add(getClass().getResource("ui/styles/Alert_Dialogs_Style.css").toExternalForm());
+    }
+
+    private void ipPickerDialogCustomize(TextInputDialog ipPicker, Alert ipError) {
+        applyStyles(ipPicker, ipError);
+        ipPicker.setHeaderText("Please Enter Server IP");
+        ipPicker.setTitle("Connect to Server");
+        ipPicker.setContentText("IP:");
+        ipPicker.getDialogPane().setPrefSize(400, 250);
+        Image image = new Image(getClass().getResourceAsStream("ui/styles/online-game-svgrepo-com.png"));
+        ImageView imgView = new ImageView(image);
+        imgView.setFitHeight(40);
+        imgView.setFitWidth(40);
+        ipPicker.setGraphic(imgView);
+
     }
 }

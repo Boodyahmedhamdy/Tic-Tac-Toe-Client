df97f1c1eab9d71a719cbde4ef79aaaf7c491902
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-01-21 14:42:44 +0200

    removed method confilct

--- src/tictactoe/client/PlayerSocket.java
+++ src/tictactoe/client/PlayerSocket.java
@@ -247,13 +247,4 @@ public final class PlayerSocket {
 
 }*/
 
-   
-
-    public boolean isConnected() {
-        return socket != null && socket.isConnected() && !socket.isClosed();
-    }
-
-}
-
-
 

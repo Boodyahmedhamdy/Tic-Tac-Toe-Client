c296b2d9359e1226d54b8a575d2b87b70d2f0e19
Abdelrahman Mohamed Elshreif <abdelrahman31009246@f-eng.tanta.edu.eg>
2025-01-23 03:15:34 +0200

    allowing loging in again

--- src/tictactoe/client/StartOptionsScreenController.java
+++ src/tictactoe/client/StartOptionsScreenController.java
@@ -1,3 +1,124 @@
+//package tictactoe.client;
+//
+//import java.io.IOException;
+//import java.net.InetSocketAddress;
+//import java.net.Socket;
+//import java.net.URL;
+//import java.util.Optional;
+//import java.util.ResourceBundle;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import javafx.fxml.FXML;
+//import javafx.fxml.FXMLLoader;
+//import javafx.fxml.Initializable;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.TextInputDialog;
+//import javafx.scene.control.Alert;
+//import javafx.scene.control.Alert.AlertType;
+//import javafx.stage.Stage;
+//import tictactoe.client.ui.UiUtils;
+//
+//public class StartOptionsScreenController implements Initializable {
+//
+//    @FXML
+//    private Button playWithAIbtn;
+//    @FXML
+//    private Button playWithFreindBtn;
+//    @FXML
+//    private Button playOnlineBtn;
+//    @FXML
+//    private Button prevRecordsBtn;
+//
+//    private PlayerSocket playerSocket;
+//
+//    @Override
+//    public void initialize(URL url, ResourceBundle rb) {
+//        playerSocket = PlayerSocket.getInstance();
+//    }
+//
+//    @FXML
+//    private void handlePlayWithAI() {
+//        System.out.println("Play With AI");
+//    }
+//
+//    @FXML
+//    private void handlePlayWithFriend() {
+//        UiUtils.showReplayAlert("Do you want to record the game?", () -> {
+//            GameScreenController.isRecording = true;
+//            try {
+//                navigateToScreen("gameScreen.fxml", playWithFreindBtn);
+//            } catch (IOException ex) {
+//                Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }, () -> {
+//            try {
+//                GameScreenController.isRecording = false;
+//                System.out.println("Game will not be recorded.");
+//                navigateToScreen("gameScreen.fxml", playWithFreindBtn);
+//            } catch (IOException ex) {
+//                Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }, () -> {
+//            System.out.println("Dialog closed.");
+//        });
+//    }
+//
+//    @FXML
+//    private void handlePlayOnline() {
+//        TextInputDialog ipPicker = new TextInputDialog();
+//        ipPicker.setTitle("Connect to Server");
+//        ipPicker.setHeaderText("Enter Server IP Address");
+//        ipPicker.setContentText("IP Address:");
+//
+//        Optional<String> result = ipPicker.showAndWait();
+//        result.ifPresent(ip -> {
+//            if (!isValidIpAddress(ip)) {
+//                UiUtils.showValidationAlert("Invalid IP address format!");
+//                return;
+//            }
+//
+//            try {
+//                InetSocketAddress serverAddress = new InetSocketAddress(ip, 9800); // Default server port
+//                if (playerSocket.connect(serverAddress, 5000)) { // 5-second timeout
+//                    System.out.println("Connected to server at: " + ip);
+//                    navigateToScreen("LoginScreen.fxml", playOnlineBtn);
+//                } else {
+//                    UiUtils.showValidationAlert("Failed to connect to the server.");
+//                }
+//            } catch (Exception ex) {
+//                Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, null, ex);
+//                UiUtils.showValidationAlert("Error connecting to the server: " + ex.getMessage());
+//            }
+//        });
+//    }
+//
+//    @FXML
+//    private void handlePreviousRecords() {
+//        try {
+//            navigateToScreen("PreviousRecordsScreen.fxml", prevRecordsBtn);
+//        } catch (IOException ex) {
+//            Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, null, ex);
+//            UiUtils.showValidationAlert("Error navigating to previous records screen.");
+//        }
+//    }
+//
+//    private boolean isValidIpAddress(String ipAddress) {
+//        String regex = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+//        return ipAddress.matches(regex);
+//    }
+//
+//    private void navigateToScreen(String fxmlFile, Button button) throws IOException {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+//        Parent root = loader.load();
+//        Stage stage = (Stage) button.getScene().getWindow();
+//        stage.setScene(new Scene(root));
+//        stage.show();
+//    }
+//}
+
 package tictactoe.client;
 
 import java.io.IOException;
@@ -9,20 +130,25 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextInputDialog;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import tictactoe.client.PlayerSocket;
 import tictactoe.client.ui.UiUtils;
 
 public class StartOptionsScreenController implements Initializable {
 
+    @FXML
+    private Label gameTitle;
     @FXML
     private Button playWithAIbtn;
     @FXML
@@ -32,21 +158,27 @@ public class StartOptionsScreenController implements Initializable {
     @FXML
     private Button prevRecordsBtn;
 
-    private PlayerSocket playerSocket;
+    private void setupButtonActions() {
+        playWithAIbtn.setOnAction(event -> handlePlayWithAI());
+        playWithFreindBtn.setOnAction(event -> handlePlayWithFriend());
+        playOnlineBtn.setOnAction(event -> handlePlayOnline());
+        prevRecordsBtn.setOnAction(event -> handlePreviousRecords());
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        playerSocket = PlayerSocket.getInstance();
+        setupButtonActions();
+
     }
 
     @FXML
     private void handlePlayWithAI() {
         System.out.println("Play With AI");
     }
-
+    
     @FXML
     private void handlePlayWithFriend() {
-        UiUtils.showReplayAlert("Do you want to record the game?", () -> {
+        UiUtils.showReplayAlert("Do you want to record the game ?", () -> {
             GameScreenController.isRecording = true;
             try {
                 navigateToScreen("gameScreen.fxml", playWithFreindBtn);
@@ -54,67 +186,124 @@ public class StartOptionsScreenController implements Initializable {
                 Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, null, ex);
             }
         }, () -> {
+            GameScreenController.isRecording = false;
+            System.out.println("Game Will Not Be Recorded");
             try {
-                GameScreenController.isRecording = false;
-                System.out.println("Game will not be recorded.");
                 navigateToScreen("gameScreen.fxml", playWithFreindBtn);
             } catch (IOException ex) {
                 Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }, () -> {
-            System.out.println("Dialog closed.");
-        });
-    }
 
+        }, () -> {
+            System.out.println("Dialog Closed");
+        });
+
+    }
+    
     @FXML
     private void handlePlayOnline() {
-        TextInputDialog ipPicker = new TextInputDialog();
-        ipPicker.setTitle("Connect to Server");
-        ipPicker.setHeaderText("Enter Server IP Address");
-        ipPicker.setContentText("IP Address:");
+        Alert ipError = new Alert(AlertType.ERROR);
+        TextInputDialog ipPicker = new TextInputDialog("");
+        ipPickerDialogCustomize(ipPicker, ipError);
 
         Optional<String> result = ipPicker.showAndWait();
         result.ifPresent(ip -> {
             if (!isValidIpAddress(ip)) {
-                UiUtils.showValidationAlert("Invalid IP address format!");
-                return;
-            }
-
-            try {
-                InetSocketAddress serverAddress = new InetSocketAddress(ip, 9800); // Default server port
-                if (playerSocket.connect(serverAddress, 5000)) { // 5-second timeout
-                    System.out.println("Connected to server at: " + ip);
-                    navigateToScreen("LoginScreen.fxml", playOnlineBtn);
-                } else {
-                    UiUtils.showValidationAlert("Failed to connect to the server.");
+                showErrorAlert(ipError, "INVALID IP", "Connection Failed");
+            } else {
+                try {
+                    if (connectToServer(ip)) {
+                        navigateToScreen("LoginScreen.fxml", playOnlineBtn);
+                    } else {
+                        showErrorAlert(ipError, "Cannot Find Server with This IP", "Connection Failed");
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, null, ex);
                 }
-            } catch (Exception ex) {
-                Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, null, ex);
-                UiUtils.showValidationAlert("Error connecting to the server: " + ex.getMessage());
             }
         });
     }
-
+    
     @FXML
     private void handlePreviousRecords() {
         try {
             navigateToScreen("PreviousRecordsScreen.fxml", prevRecordsBtn);
         } catch (IOException ex) {
             Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, null, ex);
-            UiUtils.showValidationAlert("Error navigating to previous records screen.");
         }
     }
 
-    private boolean isValidIpAddress(String ipAddress) {
+    public boolean connectToServer(String serverIP) {
+        System.out.println("Player Initiating Connection");
+        Alert connecting = new Alert(AlertType.INFORMATION);
+        connecting.getDialogPane().getStylesheets().add(getClass().getResource("ui/styles/Alert_Dialogs_Style.css").toExternalForm());
+        connecting.setTitle("Connecting");
+        connecting.setHeaderText("Please Wait.....");
+        connecting.show();
+
+        try {
+            InetSocketAddress serverAddress = new InetSocketAddress(serverIP, 9800);
+            System.out.println("Attempting to connect to server at: " + serverAddress);
+
+            PlayerSocket socket = PlayerSocket.getInstance();
+            if (!socket.isConnected()) {
+                boolean connected = socket.connect(serverAddress, 1000); // Timeout of 1 second
+                connecting.close();
+                if (connected) {
+                    System.out.println("Successfully connected to the server.");
+                } else {
+                    System.out.println("Failed to connect to the server.");
+                }
+                return connected;
+
+            } else {
+                connecting.close();
+                System.out.println("Already connected to the server.");
+                return true;
+            }
+        } catch (Exception e) {
+            connecting.close();
+            Logger.getLogger(StartOptionsScreenController.class.getName()).log(Level.SEVERE, "Connection failed", e);
+            return false;
+        }
+    }
+
+    public boolean isValidIpAddress(String ipAddress) {
         String regex = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
         return ipAddress.matches(regex);
     }
 
-    private void navigateToScreen(String fxmlFile, Button button) throws IOException {
+    public void navigateToScreen(String fxmlFile, Button b) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
         Parent root = loader.load();
-        Stage stage = (Stage) button.getScene().getWindow();
+        Stage stage = (Stage) b.getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    public void showErrorAlert(Alert alert, String headerText, String title) {
+        alert.setAlertType(AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    }
+
+    private void applyStyles(TextInputDialog ipPicker, Alert alert) {
+        ipPicker.getDialogPane().getStylesheets().add(getClass().getResource("ui/styles/Alert_Dialogs_Style.css").toExternalForm());
+        alert.getDialogPane().getStylesheets().add(getClass().getResource("ui/styles/Alert_Dialogs_Style.css").toExternalForm());
+    }
+
+    private void ipPickerDialogCustomize(TextInputDialog ipPicker, Alert ipError) {
+        applyStyles(ipPicker, ipError);
+        ipPicker.setHeaderText("Please Enter Server IP");
+        ipPicker.setTitle("Connect to Server");
+        ipPicker.setContentText("IP:");
+        ipPicker.getDialogPane().setPrefSize(400, 250);
+        Image image = new Image(getClass().getResourceAsStream("ui/styles/online-game-svgrepo-com.png"));
+        ImageView imgView = new ImageView(image);
+        imgView.setFitHeight(40);
+        imgView.setFitWidth(40);
+        ipPicker.setGraphic(imgView);
+
     }
 }

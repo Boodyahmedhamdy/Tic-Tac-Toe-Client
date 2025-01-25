0bf45b22d9125ad7daa5c925a53327569e8fd6c7
Rofaida <rofaidasobhy123@gmail.com>
2025-01-25 10:27:22 +0200

    solve bug in playWith AI

--- src/tictactoe/client/GameScreenWithAIController.java
+++ src/tictactoe/client/GameScreenWithAIController.java
@@ -79,6 +79,7 @@ public class GameScreenWithAIController implements Initializable {
     private Label Player_human;
     @FXML
     private Label player_AI;
+    //String[][] status;
     
     
     /**
@@ -109,11 +110,7 @@ public class GameScreenWithAIController implements Initializable {
         };
         isGameOver=false;
         //setDifficulty(difficulty);
-        System.out.println("Difficulty set to: " + difficulty);
-        
-
-        
-        
+        System.out.println("Difficulty set to: " + difficulty); 
     }    
     
     
@@ -121,32 +118,36 @@ public class GameScreenWithAIController implements Initializable {
     
     @FXML
     void handleOnClick(ActionEvent event) throws IOException {
-        
-            if (isGameOver)
-                return;
-            Button clickedButton = (Button) event.getSource();
-            if(!clickedButton.getText().isEmpty()) 
-                return;
-            if (CurrentPlayer.equals(Player)) {
-                clickedButton.setText(Player);
-                //clickedButton.getStyleClass().add("o-button");
-
-                // Switch to AI player
-                CurrentPlayer=AI;
-                //player_AI.setStyle("-fx-background-color:#D3D3D3;");
-                //Player_human.setStyle("-fx-background-color:none;");
-               
-                checkWhoIsTheWinner();
-                
-                if(!isGameOver){
-                    computerMove();
-                    
-                }
-                   
+        if (isGameOver)
+            return;
+        Button clickedButton = (Button) event.getSource();
+        if(!clickedButton.getText().isEmpty()) 
+            return;
+        if (CurrentPlayer.equals(Player)) {
+            clickedButton.setText(Player);
             
-            }
-        clickedButton.setDisable(true);
+            clickedButton.setDisable(true);
+            /*System.out.println("status of board after player move: ");
+            printDisable();*/
+            printBord();
+            //clickedButton.getStyleClass().add("o-button");
 
+            // Switch to AI player
+            CurrentPlayer=AI;
+            //player_AI.setStyle("-fx-background-color:#D3D3D3;");
+            //Player_human.setStyle("-fx-background-color:none;");
+
+            checkWhoIsTheWinner();
+
+            if(!isGameOver&&CurrentPlayer.equals(AI)){
+                computerMove();
+                printBord();
+
+            }
+
+
+        }
+       
     }
    
     boolean checkWin(String symbol){
@@ -425,14 +426,36 @@ public class GameScreenWithAIController implements Initializable {
    
     private void restartGame() {
         isGameOver=false;
+        CurrentPlayer=Player;
         gridPane.getChildren().forEach((node) -> {
             ((Button) node).setText("");
             ((Button) node).setStyle("-fx-background-color: linear-gradient(to bottom, #ffffff, #f2f2f2);");
             ((Button) node).setDisable(false);
         });
+        
+        System.out.println("status of board after restart: ");
+        //printDisable();
         //CurrentPlayer=Player;
     }
     
     
     
+    private void printBord(){
+        System.out.println();
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(board[i][j].getText()+" ");
+            }
+            System.out.println();
+        }
+    }
+   /* private void printDisable(){
+        System.out.println();
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(board[i][j].isDisable()+" ");
+            }
+            System.out.println();
+        }
+    }*/
 }
